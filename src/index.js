462fceedbf9cb184a70381b0d736a5fbbdd53178
Ibrahim Elsanosi <ibrahimsabat@yahoo.com>
2021-09-14 11:22:25 +0100

    added auth 6

--- src/index.js
+++ src/index.js
@@ -18,5 +18,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // For testing
-// test Amplify ci/di
 reportWebVitals();
